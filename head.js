3e68091ea5b1864877ae7c08304a9ccc298031d2
SiriusLL <sirius.love.light@gmail.com>
2021-03-02 03:46:12 +0000

    finds first element of an array and compairs to a given number

---
+++ head.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌: ${actual} !== ${expected}`);
+  }
+};
+
+
+const head = (array) => {
+  if (array.lenght === 0) {
+    return undefined;
+  } else {
+    return array[0];
+  }
+}
+//TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Goodbye");
+assertEqual(head([1]), 5);
+assertEqual(head([]), 5);
+assertEqual(head([5]), 5);
