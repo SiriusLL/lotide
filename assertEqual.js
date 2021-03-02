685659c8bd1c523832f519a0c4e79bdff78c72f6
SiriusLL <sirius.love.light@gmail.com>
2021-03-02 02:33:44 +0000

    function that check if actual === expected value

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅: ' + actual + ' === ' + expected);
+  } else {
+    console.log('❌: ' + actual + ' !== ' + expected);
+  }
+};
+
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 'hello');
+assertEqual(1, 1);
+assertEqual(1, 10);
+assertEqual('hello', 'hello');
