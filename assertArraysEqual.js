400965fd73136339f5b5840dddf1d28e465e85ca
SiriusLL <sirius.love.light@gmail.com>
2021-03-03 02:59:44 +0000

    compairs to arrays using eqArray and prints pass or fail

---
+++ assertArraysEqual.js
@@ -0,0 +1,36 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌: actual !== expected`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+    
+  
+  } return true;
+};
+
+
+const assertArraysEqual = (array1, array2) => {
+  if(eqArrays(array1, array2)) {
+    console.log(`✅: PASS`);
+  } else {
+    console.log(`❌: FAIL`);
+  }
+};
+  
+assertArraysEqual([1, 2, 3], [1, 2, 2]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
