7046e966404b4411566ffdc339ba3271bcfde305
SiriusLL <sirius.love.light@gmail.com>
2021-03-03 05:15:46 +0000

    compair two arrays and create a new array only includes values that are not the same

---
+++ without.js
@@ -0,0 +1,49 @@
+const eqArrays = (arr1, arr2) => {
+  
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+    
+  
+  } return true;
+};
+
+
+const assertArraysEqual = (array1, array2) => {
+  if(eqArrays(array1, array2)) {
+    console.log(`✅: PASS`);
+  } else {
+    console.log(`❌: FAIL`);
+  }
+};
+
+
+const without = (arry1, arry2) => {
+  let newArr = [];
+  for (let i = 0; i < arry1.length; i++) {
+    if(!arry2.includes(arry1[i])) {
+      newArr.push(arry1[i]);
+    
+      
+    }
+
+    
+    
+    
+  } console.log(newArr)
+  return newArr;
+}
+
+/*without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+//assertArraysEqual(without([1,2,3], [1]), [2,3])
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);*/
