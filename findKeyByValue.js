2f2f7b5c475c8bdae0504d184857e366f138a0e4
SiriusLL <sirius.love.light@gmail.com>
2021-03-04 21:05:01 +0000

    comopairs arrays within an object

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,24 +6,29 @@ const assertArraysEqual = (array1, array2) => {
   }
 };
 
-
-
-findKeysByValue = function(bestShows, shows) {
-  newArr = object.keys(bestShows);
-  console.log(newArr);
+const eqArrays = (arr1, arr2) => {
+  
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+    
+  
+  } return true;
 }
 
-
-
-
-
-
-
-
-
-
-
-
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌: actual !== expected`);
+  }
+};
 
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
@@ -31,5 +36,28 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
+findKeyByValue = function(bestShows, shows) {
+  //const newArr = Object.keys(bestShows);
+  let result = '';
+  for (let genreIndex in bestShows) {
+    if (bestShows[genreIndex] === shows) {
+      console.log(bestShows[genreIndex]);
+      result = genreIndex;
+      return result;
+    } //else {
+      //continue;
+    //}
+  }
+  return; // undefined;
+
+};
+
+//console.log(result);
+
+
+
+ //console.log(bestTVShowsByGenre['drama']);
+
+//assertArraysEqual(findKeysByValue(bestTVShowsByGenre, "The Wire"), "The Wire");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
