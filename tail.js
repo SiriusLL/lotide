14e74ed8a437b29d672a4c1ee8eede8006a79fca
SiriusLL <sirius.love.light@gmail.com>
2021-03-08 22:15:13 +0000

    added export module

--- tail.js
+++ tail.js
@@ -12,10 +12,4 @@ const tail = (array) => {
   return tailArray;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const tailWords = tail(words);
-//tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tailWords[0], "Lighthouse"); // checks first element is light house
-assertEqual(tailWords[1], "Labs"); // checks first element is light house
+module.exports = tail;
