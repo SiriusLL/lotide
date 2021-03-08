812468d4c3ce933212d7b49b194a0904002c57a7
SiriusLL <sirius.love.light@gmail.com>
2021-03-08 20:21:56 +0000

    modified to allow tests from a separate folder modularizing it

--- assertEqual.js
+++ assertEqual.js
@@ -7,9 +7,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 'hello');
-assertEqual(1, 1);
-assertEqual(1, 10);
-assertEqual('hello', 'hello');
+
+module.exports = assertEqual;
+
+
